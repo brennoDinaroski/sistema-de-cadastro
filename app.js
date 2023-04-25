31e18f0f511e3192b466a1802d3df1a0698914d9
brennoDinaroski <124304153+brennoDinaroski@users.noreply.github.com>
2023-04-25 12:02:24 -0300

    adicionando console log

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("A aplicação esta se conectando com o banco de dados")
 //mais um console log de teste:
 console.log("O paulo fez esse commit sem voce ver na aula!")
-
+console.log("Enviando para o GitHUb")
